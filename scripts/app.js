424da590ec0229472e7ae49de9703affe97360a0
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-06-11 11:53:40 +0200

    Application Routing configured

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,9 @@
-angular.module("whatapop", []);
+angular.module("whatapop", ["ngComponentRouter"]);
 
+// Configuring $locationProvider
+angular.module("whatapop").config(function($locationProvider) {
+    $locationProvider.html5Mode(true);
+});
+
+// Set root component
+angular.module("whatapop").value("$routerRootComponent","mainComponent");
--- scripts/components/MainComponent.js
+++ scripts/components/MainComponent.js
@@ -1,3 +1,13 @@
 angular.module("whatapop").component("mainComponent", {
-    templateUrl: "views/MainComponent.html"
+    templateUrl: "views/MainComponent.html",
+    $routeConfig: [{
+        name: "Products",
+        path: "/products",
+        component: "products",
+        useAsDefault: true
+    }, {
+        name: "Detail",
+        path: "/product-detail/:id",
+        component: "detail"
+    }]
 });
