feba58b0ea4b33617f91887061d37445f6771b02
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-06-14 21:06:58 +0200

    Products component modified that apply the filters to the list of products when they arrive by parameter

--- scripts/components/Products.js
+++ scripts/components/Products.js
@@ -1,16 +1,29 @@
 angular.module("whatapop").component("products", {
     templateUrl: "views/Products.html",
-    controller: ["ProductsService", function(ProductsService) {
+    controller: ["ProductsService", "$rootRouter", function(ProductsService, $rootRouter) {
         var ctrl = this;
+
         ctrl.products = [];
 
-        ctrl.$onInit = function () {
-            ProductsService.getProducts().then(function (result) {
+        ctrl.$routerOnActivate = function(next, previous) {
+            ctrl.filterCategoty = next.params.catId ? parseInt(next.params.catId) : null;
+            ctrl.categoryName = decodeURIComponent(next.params.catName);
+            ctrl.filterText = next.params.search && next.params.search != "null" ? decodeURIComponent(next.params.search) : null;
+
+            ProductsService.getProducts(ctrl.filterText, ctrl.filterCategoty).then(function (result) {
                 ctrl.products = result.data;
             }, function (err) {
                 alert(err.data);
                 console.log(err);
             });
+        };
+
+        ctrl.deleteFilterCategory = function () {
+            $rootRouter.navigate(['Products', {search: encodeURIComponent(ctrl.filterText), catId: "0", catName: ""}]);
+        };
+
+        ctrl.deleteFilterText = function () {
+            $rootRouter.navigate(['Products', {search: "", catId: "" + ctrl.filterCategoty, catName: encodeURIComponent(ctrl.categoryName)}]);
         };
     }]
 });
