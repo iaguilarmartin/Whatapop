f5e45709b58ff0d3ef2d70b50e18a06022639ad1
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-06-14 21:05:10 +0200

    Modified main component to include in the navbar a form to perform the products filter by category and name. When the filter is applied navigation is made to the product lists page

--- scripts/components/MainComponent.js
+++ scripts/components/MainComponent.js
@@ -9,5 +9,36 @@ angular.module("whatapop").component("mainComponent", {
         name: "Detail",
         path: "/product-detail/:id",
         component: "detail"
+    }],
+    controller: ["CategoriesService", "$rootRouter", function (CategoriesService, $rootRouter) {
+        ctrl = this;
+
+        var emptyCategory = {name:"Cualquier categoria", id:0, label: "Categoria"};
+
+        ctrl.text = "";
+        ctrl.selectedCategory = emptyCategory;
+        ctrl.categories = [];
+
+        ctrl.$onInit = function(next, previous) {
+            CategoriesService.getCategories().then(function (result) {
+                ctrl.categories = result.data;
+                ctrl.categories.unshift(emptyCategory);
+
+            }, function (err) {
+                alert(err.data);
+                console.log(err);
+            })
+        }
+
+        ctrl.search = function () {
+            var search = encodeURIComponent(ctrl.text);
+            var catId = "" + ctrl.selectedCategory.id;
+            var catName = encodeURIComponent(ctrl.selectedCategory.name);
+
+            ctrl.selectedCategory = emptyCategory;
+            ctrl.text = "";
+
+            $rootRouter.navigate(['Products', {search: search, catId: catId, catName: catName}]);
+        }
     }]
 });
