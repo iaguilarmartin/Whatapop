f16e81ccc4476a5ddfbfd60366c5258c6f77c736
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-06-14 21:06:17 +0200

    Product service modified to admit the filters by parameter for the getProducts function

--- scripts/services/ProductsService.js
+++ scripts/services/ProductsService.js
@@ -1,5 +1,24 @@
-angular.module("whatapop").service("ProductsService", ["$http", "AppSettings", function ($http, AppSettings) {
-    this.getProducts = function () {
-        return $http.get(AppSettings.urlAPIServer + AppSettings.productsMethod);
+angular.module("whatapop").service("ProductsService", ["$http", "AppSettings", "$q", function ($http, AppSettings, $q) {
+    this.getProducts = function (text, category) {
+        var deferred = $q.defer();
+        $http.get(AppSettings.urlAPIServer + AppSettings.productsMethod).then(function (result) {
+             var filteredResult = result.data.filter(function (product) {
+
+                if (text && product.name.toUpperCase().indexOf(text.toUpperCase()) == -1) {
+                    return false;
+                }
+
+                if (category == 0 || product.category.id == category) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+            deferred.resolve({data:filteredResult});
+        }, function (err) {
+            deferred.reject(err);
+        });
+        return deferred.promise;
     }
 }]);
