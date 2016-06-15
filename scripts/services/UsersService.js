1fdbcd26bfc269dea5ed82ea9e98e65020588a8c
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-06-15 20:10:15 +0200

    UsersService getNearUsers method modified to recibe as a parameter a coordinates object instead of latitude and longitude

--- scripts/services/UsersService.js
+++ scripts/services/UsersService.js
@@ -4,25 +4,20 @@ angular.module("whatapop").service("UsersService", ["$http", "AppSettings", "$q"
     function ($http, AppSettings, $q, $filter, $haversine) {
 
         // Return all the users that are within a distance lower than the specified
-        this.getNearUsers = function (latitude, longitude, distance) {
-
-            var coordOrigin = {
-                "latitude": latitude,
-                "longitude": longitude
-            };
+        this.getNearUsers = function (coords, distance) {
 
             // A deferred object is created to simulate that the filtering procces is done on the server side
             var deferred = $q.defer();
             $http.get(AppSettings.urlAPIServer + AppSettings.usersMethod).then(function (result) {
                 var filteredResult = $filter("filter")(result.data, function (user) {
 
-                    var coordDestination = {
+                    var userCoords = {
                         "latitude": user.latitude,
                         "longitude": user.longitude
                     };
 
                     // Filtering users that are near current location
-                     if ($haversine.distance(coordOrigin, coordDestination) <= distance) {
+                     if (distance == 0 || $haversine.distance(coords, userCoords) <= distance) {
                         return true;
                      } else {
                          return false;
