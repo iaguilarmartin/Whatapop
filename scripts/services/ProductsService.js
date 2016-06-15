168cda891ae9bbc4c081d941f0e1c39095bc7158
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-06-15 12:43:29 +0200

    Filtering results using Filter AngularJS filter

--- scripts/services/ProductsService.js
+++ scripts/services/ProductsService.js
@@ -1,38 +1,35 @@
-angular.module("whatapop").service("ProductsService", ["$http", "AppSettings", "$q", function ($http, AppSettings, $q) {
-    this.getProducts = function (text, category) {
-        var deferred = $q.defer();
-        $http.get(AppSettings.urlAPIServer + AppSettings.productsMethod).then(function (result) {
-             var filteredResult = result.data.filter(function (product) {
+angular.module("whatapop").service("ProductsService", ["$http", "AppSettings", "$q", "$filter",
+    function ($http, AppSettings, $q, $filter) {
+        this.getProducts = function (text, category) {
+            var deferred = $q.defer();
+            $http.get(AppSettings.urlAPIServer + AppSettings.productsMethod).then(function (result) {
+                var filteredResult = $filter("filter")(result.data, function (product) {
+                     if (text && product.name.toUpperCase().indexOf(text.toUpperCase()) == -1) {
+                        return false;
+                     }
 
-                if (text && product.name.toUpperCase().indexOf(text.toUpperCase()) == -1) {
-                    return false;
-                }
-
-                if (!category || product.category.id == category) {
-                    return true;
-                } else {
-                    return false;
-                }
+                     if (!category || product.category.id == category) {
+                        return true;
+                     } else {
+                        return false;
+                     }
+                 });
+                deferred.resolve({data:filteredResult});
+            }, function (err) {
+                deferred.reject(err);
             });
+            return deferred.promise;
+        }
 
-            deferred.resolve({data:filteredResult});
-        }, function (err) {
-            deferred.reject(err);
-        });
-        return deferred.promise;
-    }
-
-    this.getProductById = function (id) {
-        var deferred = $q.defer();
-        $http.get(AppSettings.urlAPIServer + AppSettings.productsMethod).then(function (result) {
-            var filteredResult = result.data.filter(function (product) {
-                return product.id == id;
+        this.getProductById = function (id) {
+            var deferred = $q.defer();
+            $http.get(AppSettings.urlAPIServer + AppSettings.productsMethod).then(function (result) {
+                var filteredResult = $filter("filter")(result.data, {"id": id});
+                deferred.resolve({data:filteredResult});
+            }, function (err) {
+                deferred.reject(err);
             });
-
-            deferred.resolve({data:filteredResult});
-        }, function (err) {
-            deferred.reject(err);
-        });
-        return deferred.promise;
+            return deferred.promise;
+        }
     }
-}]);
+]);
