1f0cac38855d478bc25b623019588ac81562ffae
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2016-06-15 12:14:30 +0200

    PhotoFilter modified so it can be used with strings and with arreys

--- scripts/filters/PhotoFilter.js
+++ scripts/filters/PhotoFilter.js
@@ -1,9 +1,12 @@
 angular.module("whatapop").filter("PhotoFilter",["AppSettings", function (AppSettings) {
 
     return function (images) {
-        console.log(images);
         if (images && images.length > 0) {
-            return AppSettings.urlAPIServer + images[0];
+            if (typeof images === 'string' ) {
+                return AppSettings.urlAPIServer + images;
+            } else {
+                return AppSettings.urlAPIServer + images[0];
+            }
         } else {
             return "";
         }
